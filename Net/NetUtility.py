fe81ccb3cd86ec03db05f88622344730d7fabd29
EricWang <wwssllabcd@gmail.com>
2021-05-31 00:48:01 +0800

    add recv_ack/send_ack

--- Net/NetUtility.py
+++ Net/NetUtility.py
@@ -11,8 +11,18 @@ class NetUtility:
         sk.bind((ip, port))
         sk.listen()
         return sk
+    
+    def recv_ack(self, conn):
+        conn.recv(2)
+        
+    def send_ack(self, conn):
+        msg = 'ok'.encode(encoding="utf-8")
+        conn.send(msg)
 
-    def receive_data_to_file(self, fileName, fileSize, conn):
+    def receive_file(self, fileName, fileSize, conn):
+        if fileSize == 0:
+            return
+        
         orgSize = fileSize
         with open(fileName, mode='wb') as f:
             while fileSize > 0:
@@ -23,8 +33,23 @@ class NetUtility:
 
                 fileSize -= len(buffer)
                 f.write(buffer)
+    
+    def receive_file_with_ack(self, fileName, fileSize, conn):
+        if fileSize == 0:
+            return
+        self.receive_file(fileName, fileSize, conn)
+        self.send_ack(conn)
 
     def send_file(self, filePath, fileSize, conn):
+        if fileSize == 0:
+            return
         with open(filePath, mode='rb') as f:
-            if fileSize > 0:
-                conn.sendall(f.read())
+            conn.sendall(f.read())
+
+    def send_file_with_ack(self, filePath, fileSize, conn):
+        if fileSize == 0:
+            return
+        self.send_file(filePath, fileSize, conn)
+        self.recv_ack(conn)
+
+        
