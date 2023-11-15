90a9ffeb210180d2158164380b6b08d9e0895a94
EricWang <wwssllabcd@gmail.com>
2023-11-15 15:04:31 +0800

    add cookie parameter in get_url_data()

--- Utility/UrlUtility.py
+++ Utility/UrlUtility.py
@@ -4,26 +4,24 @@ import urllib.request
 
 
 class UrlUtility:
-    def get_url_data(self, url):
-        #header={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0'}
-   
+    def get_url_data(self, url, cookies = None):
         req = urllib.request.Request(url)
-        #req.add_header('user-agent','Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11')
-        req.add_header('user-agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0')
 
-        req = urllib.request.urlopen(url)
+        if cookies != None:
+            req.add_header('Cookie', cookies)
 
-        if req.url != url:
-           return self.get_url_data(req.url)
-            
+        response = urllib.request.urlopen(req)
 
-        print(req.url)
-        print(req.status)
-        encoding = req.headers.get_content_charset() 
-        bytecode = req.read()
+        # check is redirect or not?
+        if response.url != url:
+            return self.get_url_data_2(response.url, cookies)
+
+
+        encoding = response.headers.get_content_charset() 
+        bytecode = response.read()
         data = bytecode.decode(encoding, errors='ignore')
-
         return data
+    
 
     def set_utf8(self, data):
         str = "charset="
