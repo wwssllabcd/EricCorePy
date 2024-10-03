de28de4401f1ffe10371d58c0da509aedc5e52fa
EricWang <wwssllabcd@gmail.com>
2024-10-03 14:28:37 +0800

    mod msg

--- Utility/M3u8.py
+++ Utility/M3u8.py
@@ -6,6 +6,9 @@ class M3u8:
         return "#EXTM3U" + m_crlf
 
     def get_m3u8_item_string(self, title, url):
-        res = '#EXTINF:-1  group-title="liveNews" tvg-logo="N/A", ' + title + m_crlf
+        res = '#EXTINF:-1  group-title="Live_News" tvg-logo="N/A", ' + title + m_crlf
         res += url + m_crlf
+
+        
+
         return res
