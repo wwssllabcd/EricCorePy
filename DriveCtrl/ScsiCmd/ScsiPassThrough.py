f6d0919603a0708b0d1da1ad671d0b2f099593f4
EricWang <wwssllabcd@gmail.com>
2024-10-26 00:57:41 +0800

    add check

--- DriveCtrl/ScsiCmd/ScsiPassThrough.py
+++ DriveCtrl/ScsiCmd/ScsiPassThrough.py
@@ -6,6 +6,11 @@ def send_scsi_cmd(handle, cmd: ScsiCmdBase, writeBuffer=None):
     if cmd.direct == SCSI_IOCTL_DATA_IN:
         byteArray = bytearray(cmd.dataLen)
     else:
+        if type(writeBuffer) == list:
+            # check list len for performance issue
+            if len(writeBuffer) >= 4096:
+                print("conver to bytearray, but list len too big = " + str(len(writeBuffer)))
+
         # try to convert to bytearray, include list
         byteArray = bytearray(writeBuffer)
 
